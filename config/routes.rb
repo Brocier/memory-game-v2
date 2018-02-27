d397269ff4c7bb448ca2bf720bcba9a32f29a490
Brocier <jschoe04@gmail.com>
2018-02-27 14:11:40 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-
+  namespace :api do
+    resources :users do
+      resources :high_scores
+    end
+  end
+  namespace :api do
+    resources :games do
+      resources :high_scores
+    end
+  end
 end
