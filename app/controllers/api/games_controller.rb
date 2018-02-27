3e1dfa70fdb10a8bdfba7becbafbec35ec73c187
Brocier <jschoe04@gmail.com>
2018-02-27 14:22:44 -0500

    Controllers set

---
+++ app/controllers/api/games_controller.rb
@@ -0,0 +1,37 @@
+class Api::GamesController < ApplicationController
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def create
+    @game = Game.create!(game_params)
+
+    render json: @game
+  end
+
+  def show
+    @game = Game.find(params[:id])
+
+    render json: @game
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update!(game_params)
+
+    render json: @game
+  end
+
+  def destroy
+    @game = Game.find(params[:id]).delete
+
+    render status: :ok
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:details)
+  end
+end
