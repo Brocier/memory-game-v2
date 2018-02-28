92fa65cc92c3185a2a33379900e42f828e0b9126
Brocier <jschoe04@gmail.com>
2018-02-28 11:07:06 -0500

    adding multiple users to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@ ActiveRecord::Base.connection.tables.each do |t|
     ActiveRecord::Base.connection.reset_pk_sequence!(t)
   end
 
-josh = User.create(name: 'Josh', image: 'https://i.imgur.com/XT6bwJI.jpg?1')
-match = Game.create(details: 'Match cards')
+match = Game.create(details: 'Match cards as quickly as possible')
+
+josh = User.create(name: 'Josh', image: 'https://i.imgur.com/B0KOrC1.jpg?2')
 Score.create(score: 100, user: josh, game: match)
+
+beth = User.create(name: 'Beth', image: 'https://i.imgur.com/1ruSz1K.jpg')
+Score.create(score: 10000, user: beth, game: match)
+
+caleb = User.create(name: 'Caleb', image: 'https://i.imgur.com/mEaMhP7.jpg')
+Score.create(score: 3, user: caleb, game: match)
