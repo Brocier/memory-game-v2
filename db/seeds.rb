a88a9df349bc56a187d56aaa76fad1e728fce534
Brocier <jschoe04@gmail.com>
2018-02-28 11:29:50 -0500

    Added difficulty to the games

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@ ActiveRecord::Base.connection.tables.each do |t|
     ActiveRecord::Base.connection.reset_pk_sequence!(t)
   end
 
-match = Game.create(details: 'Match cards as quickly as possible')
+matchEasy = Game.create(details: 'Match cards as quickly as possible' difficulty: 'SuperEasy - 4 Cards to match')
 
 josh = User.create(name: 'Josh', image: 'https://i.imgur.com/B0KOrC1.jpg?2')
-Score.create(score: 100, user: josh, game: match)
+Score.create(score: 100, user: josh, game: matchEasy)
 
 beth = User.create(name: 'Beth', image: 'https://i.imgur.com/1ruSz1K.jpg')
-Score.create(score: 10000, user: beth, game: match)
+Score.create(score: 10000, user: beth, game: matchEasy)
 
 caleb = User.create(name: 'Caleb', image: 'https://i.imgur.com/mEaMhP7.jpg')
-Score.create(score: 3, user: caleb, game: match)
+Score.create(score: 3, user: caleb, game: matchEasy)
