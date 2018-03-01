8a37e48bc9aae427eac473d556759aa87bb71a35
Brocier <jschoe04@gmail.com>
2018-03-01 11:20:09 -0500

    removed dotenv requirement

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import registerServiceWorker from './registerServiceWorker';
-require('dotenv').config()
 
 ReactDOM.render(
   <Root/>, document.getElementById('root'));
