8cf7e67c6ad90a45427fec0f2576d43d4e92efc1
Brocier <jschoe04@gmail.com>
2018-03-01 23:05:21 -0500

    spinning card animation

--- client/src/components/SplashPage.js
+++ client/src/components/SplashPage.js
@@ -9,9 +9,26 @@ class SplashPage extends Component {
         <h1>
           Memory Game v2
         </h1>
-        <button onClick={() => this.props.push(`/users`)}>
-          Log In
-        </button>
+        <div>
+          <button onClick={() => this.props.push(`/users`)}>
+            Log In
+          </button>
+        </div>
+        <CardAnimation className="wrapper">
+          <div className="card">
+            <div className="club1">&#9827;</div>
+            <div className="ace1">
+              <div className="ace_a1">A
+                <br/>
+                <div className="ace_club1">&#9827;</div>
+              </div>
+              {/* <div class="ace2"></div>
+              <div class="ace_a2">A</div>
+              <br/>
+              <div class="ace_club2">&#9827;</div> */}
+            </div>
+          </div>
+        </CardAnimation>
       </SplashPageContainer>
     )
   }
@@ -19,10 +36,83 @@ class SplashPage extends Component {
 
 export default connect(null, {push})(SplashPage)
 
+const CardAnimation = styled.div `
+.card {
+  -webkit-user-select: none; 
+  -moz-user-select: none;
+  -ms-user-select: none;
+  cursor: default;
+  width: 200px;
+  height: 350px;
+  color: #000033;
+  background-color: #a8f4b1;
+  border-radius: 15px;
+  text-align: center;
+  line-height: 350px;
+  font-size: 100px;
+  -webkit-animation: rotate 4s linear infinite;
+  -moz-animation: rotate 4s linear infinite;
+  animation: rotate 4s linear infinite;
+  margin: 0 auto;
+  position: relative;
+  top: 70px;
+}
+
+.ace1 {
+  position: relative;
+  font-size: 30px;
+  top: -340px; 
+  left: -80px;
+  line-height: 20px;
+}
+
+.ace_club1 {
+  position: relative;
+  left: 0px;
+  top: 7px;
+}
+
+.ace2 {
+  position: relative;
+  font-size: 30px;
+  top: 250px; 
+  left: 155px;
+  line-height: 20px;
+  -webkit-transform: rotate(180deg);
+  -moz-transform: rotate(180deg);
+  -o-transform: rotate(180deg);
+  transform: rotate(180deg);
+}
+
+.ace_club2{
+  position: relative;
+  left: -5px;
+  top: 5px;
+}
+
+.wrapper {
+  -webkit-perspective: 25em;
+  -moz-perspective: 25em;
+  perspective: 25em;
+}
+
+@-webkit-keyframes rotate {
+  100% {-webkit-transform: rotatey(360deg);}
+}
+
+@-moz-keyframes rotate {
+  100% {-moz-transform: rotatey(360deg);}
+}
+
+@keyframes rotate {
+  100% {transform: rotatey(360deg);}
+}
+`
+
 const SplashPageContainer = styled.div `
     display: flex;
     flex-direction:column;
-    justify-content:center;
+    justify-content:space-around;
     align-items:center;
     width:100vw;
     height:100vh;
@@ -31,7 +121,6 @@ const SplashPageContainer = styled.div `
     right:0;
     bottom:0;
     font-family: 'Montserrat', sans-serif;
-    background-color: #B3B3B3;
     color:#000033;
     font-size: 45px;
     text-shadow:4px 4px 8px rgba(0,0,0,0.35);
@@ -39,7 +128,6 @@ const SplashPageContainer = styled.div `
     background-repeat:no-repeat;
     h1{
       padding: 20px;
-      position: absolute;
       top:65px;
       margin:20px;
       letter-spacing: 1px;
@@ -64,7 +152,6 @@ const SplashPageContainer = styled.div `
     button{
       border:1px solid black;
       background:none;
-      margin-left: 20px;
       background:rgba(255,255,255,0.35);
       box-shadow:4px 4px 8px rgba(0,0,0,0.35);
       width:100px;
