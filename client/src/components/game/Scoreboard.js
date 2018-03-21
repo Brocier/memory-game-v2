b8a4977998451f7c10b023ba17e52f15e961d7f9
Brocier <jschoe04@gmail.com>
2018-03-21 19:58:30 -0400

    changed styling on the scoreboard

--- client/src/components/game/Scoreboard.js
+++ client/src/components/game/Scoreboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getScoresRoute} from '../../actions/thunk.scores.js'
+import styled from 'styled-components'
 
 class Scoreboard extends Component {
   componentWillMount() {
@@ -12,23 +13,17 @@ class Scoreboard extends Component {
 
     console.log("SCORES", this.props.scores)
     return (
-      <div>Scoreboard
-        <div>
-          {this
-            .props
-            .scores
-            .map((score, i) => {
-              return (
-                <div key={i}>
-                  <div>
-                    &#9679;{score.score}
-                  </div>
-                </div>
-              )
-            })}
-        </div>
-
-      </div>
+      <ScoreBoardContainer>Scoreboard {this
+          .props
+          .scores
+          .map((score, i) => {
+            return (
+              <div key={i}>
+                &#9679;{score.score}
+              </div>
+            )
+          })}
+      </ScoreBoardContainer>
     );
   }
 }
@@ -37,3 +32,8 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps, {getScoresRoute})(Scoreboard);
+
+const ScoreBoardContainer = styled.div `
+display: flex;
+flex-wrap: wrap;
+`
